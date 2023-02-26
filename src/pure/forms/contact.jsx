6f2c12d3dadf0ded62fd267f66298fbe99b7fc1a
Lucas Sosa <89560645+lucassosa23@users.noreply.github.com>
2023-02-26 20:02:51 -0300

    Add files via upload

--- src/pure/forms/contact.jsx
+++ src/pure/forms/contact.jsx
@@ -3,20 +3,76 @@ import Proptypes from "prop-types";
 import {Contact} from "../../models/contact.random"
 
 
-export default function  ContactComponent(contact) {
-  return (
-    <div>
 
-        <h1>Nombre: {contact.name}</h1>
-        <h2>Apellido : {contact.apellido}</h2>
-        <h3>Email: {contact.email}</h3>
-        <h4>Conectado: {contact.conectado ? "Contacto En Línea" : "Contacto No Disponible"}</h4>
-    </div>
+
+
+
+const  ContactComponent = ({contact, completed, remove}) => {
+  
+
+  
+  function ContactCompleted (){
+    if (contact.conectado) {
+      return(<i onClick={() => completed(contact)} className='bi-toggle-on task-action' style={{color: "green"}}></i>);
+     
+     }else{ 
+          return(<i  onClick={() =>completed(contact)} className='bi-toggle-off task-action' style={{color: "red "}}></i>);
+      }
+    };
+  
+    return (
+      
+    <table className='table'>
+        <thead>
+        <tr className='ms-2'>
+          <th> 
+              <span className='name-contact'> Nombre: {contact.name}</span>
+          </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+        <td className='align-middle'>
+          <span className='apellido-contact'> Apellido: {contact.apellido}</span>
+        </td>
+        </tr>
+        <tr>
+        <td className='align-middle'>
+          <span className='email-contact'> Email: {contact.email}</span>
+        </td>
+        </tr>
+        <tr>
+        <td className='align-middle'>
+          {ContactCompleted()}
+          
+          <span className='estado-contact'> Estado :</span>
+          <i className='bi-trash task-action' style={{color: "red"}} onClick={() => remove(contact)}></i>
+          
+          
+      
+        </td>
+        </tr>
+        
+
+        </tbody>
+    
+   
+      
+
+     </table>
   )
-};
 
-
+  
+    }
 ContactComponent.propTypes = {
 
-    contact: Proptypes.instanceOf(Contact)
+    contact: Proptypes.instanceOf(Contact).isRequired,
+    completed: Proptypes.func.isRequired,
+    remove: Proptypes.func.isRequired
+
+
     };
+
+
+
+    export default ContactComponent;
